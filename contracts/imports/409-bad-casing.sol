bc81aa8c78304ec75dc4ae9b34955c0fc9345c15
John Kane <john@kanej.me>
2022-09-07 09:53:39 +0100

    fix outside project example

--- contracts/imports/409-bad-casing.sol
+++ contracts/imports/409-bad-casing.sol
@@ -1,6 +1,4 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-// Update the link to a sol file outside of
-// the workspace folder
-import "../../../sample/contracts/Lock.sol"; // 409
+// import "../access/auth.sol"; // 409
